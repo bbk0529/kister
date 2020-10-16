137dd9d857c52f50323deef1024571a3d0cda829
Park <bbk0529@gmail.com>
2020-10-16 14:19:44 +0200

    updated template in connection with the data

--- water/views.py
+++ water/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Grundwasserstand
 
 def wasserstand_list(request):
-    return render(request,'water/wasserstand_list.html', {})
+    wasserstand = Grundwasserstand.objects.all()
+    print("*"*100)
+    print(wasserstand)
+    print("*"*100)
+    return render(request,'water/wasserstand_list.html', {'stands': wasserstand})
